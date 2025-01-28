f45b3afda81cdf3b609841815381b0782b978ebe
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 16:38:13 +0300

    Add files via upload

---
+++ lab4/dop2.0/dop2.0.cpp
@@ -0,0 +1,33 @@
+﻿#include <iomanip> 
+#include <iostream>
+void main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	char c, probel = ' ';
+	cout << "Введите символ: "; cin >> c;
+	//1
+	cout << setw(4) << setfill(probel) << probel;
+	cout << setw(4) << setfill(c) << c << endl;
+	//2
+	cout << setw(2) << setfill(probel) << probel;
+	cout << setw(6) << setfill(c) << c << endl;
+	//3
+	cout << setw(1) << setfill(probel) << probel;
+	cout << setw(7) << setfill(c) << c << endl;
+	//4
+	cout << setw(8) << setfill(c) << c << endl;
+	//5
+	cout << setw(8) << setfill(c) << c << endl;
+	//6
+	cout << setw(8) << setfill(c) << c << endl;
+	//7
+	cout << setw(1) << setfill(probel) << probel;
+	cout << setw(7) << setfill(c) << c << endl;
+	//8
+	cout << setw(2) << setfill(probel) << probel;
+	cout << setw(6) << setfill(c) << c << endl;
+	//9
+	cout << setw(4) << setfill(probel) << probel;
+	cout << setw(4) << setfill(c) << c << endl;
+}
---
+++ lab4/dop2.2/dop2.2.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#include<cmath>
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	double d,s;
+	printf("Введите длину диагонали квадрата: \n");
+	scanf_s("%lf", &d);
+	s = (pow(d, 2)) / 2;
+	printf("Площадь квадрата равна: ");
+	printf("%.2f", s);
+}
