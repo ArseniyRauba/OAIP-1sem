0bfbfa5cc500082aed9f8a41487f787a42143705
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 16:37:11 +0300

    Add files via upload

---
+++ lab4/dop1.2/dop1.2.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+#include<locale.h>
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	double a, b, c;
+	printf("Введите переменные a: \n");
+	scanf_s("%lf", &a);
+	printf("Введите переменные b: \n");
+	scanf_s("%lf", &b);
+	c = a;
+	a = b;
+	b = c;
+	printf("a= ", a);
+	printf("%.f\n",a);
+	printf("b= ");
+	printf("%.f",c);
+} 
---
+++ lab4/dop1.3/dop1.3.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+#include<locale.h>
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	double a, b;
+	printf("Введите переменные a: \n");
+	scanf_s("%lf", &a);
+	printf("Введите переменные b: \n");
+	scanf_s("%lf", &b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("a= ", a);
+	printf("%.f\n", a);
+	printf("b= ");
+	printf("%.f",b );
+}
