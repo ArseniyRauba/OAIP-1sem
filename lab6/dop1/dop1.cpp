74afffe00b8ec7be9291b6212d80f0bb680f50b2
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 16:46:26 +0300

    Add files via upload

---
+++ lab6/dop1/dop1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double firstprice, lastprice, i;
+	setlocale(LC_ALL, "rus");
+	cout << "Введите начальную цену: ";
+	cin >> firstprice;
+	cout << "Введите конечную цену: ";
+	cin >> lastprice;
+	i = 0;
+	while(firstprice < lastprice)
+	{
+		firstprice = firstprice + firstprice * 0.03;
+		i = i + 1;
+	}
+	cout << "Конечная цена: " << firstprice << endl;
+	cout << "Количество дней: " << i << endl;
+}
---
+++ lab6/dop2/dop2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double years, procent, sum = 0, s;
+    cout << "Введите количество лет: " << endl;
+    cin >> years;
+    cout << "Введите процент: " << endl;
+    cin >> procent;
+    procent = procent / 100;
+    for (int i = 1; i <= years; i++)
+    {
+        cout << "Введите " << i << " число: " << endl;
+        cin >> s;
+        for (int j = 1; j <= years - i; j++){
+            s = s - s * procent;
+        }
+        sum = sum + s;
+    }
+    cout << "Стоимость накопленного оборудования: " << sum << endl;
+}
