04913a62684b6b8394826e29277fe73d67b8ab0f
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 16:31:12 +0300

    Add files via upload

---
+++ lab13/dop5/dop5.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void main()
+{
+	string a[50], c[50];
+	char b[100], d[100];
+	int i, j, k1 = 0, k2 = 0, count = 0, prev = 0;
+	gets_s(b, 99);
+	gets_s(d, 99);
+
+	for (i = 0; i <= strlen(b); i++)
+	{
+		if (b[i] == ' ')
+		{
+			count = prev;
+			while (count <= i)
+			{
+
+				a[k1] += b[count];
+				count++;
+			}
+			k1 += 1;
+			prev = i;
+		}
+	}
+
+	prev = 0;
+
+
+	for (i = 0; i <= strlen(d); i++)
+	{
+		if (d[i] == ' ')
+		{
+			count = prev;
+			while (count <= i)
+			{
+
+				c[k2] += d[count];
+				count++;
+			}
+			k2 += 1;
+			prev = i;
+		}
+	}
+
+	for (i = 0; i <= k1; i++)
+	{
+		for (j = i; j <= k1; j++)
+		{
+			if (size(a[i]) > size(a[j])) swap(a[i], a[j]);
+		}
+	}
+
+	for (i = 0; i <= k1; i++)
+	{
+		count = 0;
+		for (j = 0; j <= k2; j++)
+		{
+			if (a[i] == c[j]) count += 1;
+		}
+		if (count == 0)
+		{
+			cout << a[i];
+			break;
+		}
+	}
+
+}
---
+++ lab13/lab13/lab13.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	const int size = 1000;
+	char* str1 = new char[size];
+	char* str2 = new char[size];
+	int num;
+	
+	cout<<"Введите первую строку: ";
+	cin.getline(str1,size);
+	cout<<"Введите вторую строку: ";
+	cin.getline(str2,size);
+	cout<<"Введите номер позиции: ";
+	cin>>num;
+	
+
+}
