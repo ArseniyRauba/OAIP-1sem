c964c1f7228fbf441858e7a850a2be6e7fe191b9
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 16:33:08 +0300

    Add files via upload

---
+++ lab15/lab15/lab15/lab15.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stdio.h>	
+#include <stdlib.h>
+
+void main()
+{
+	setlocale(LC_CTYPE, "russian");
+	using namespace std;
+	int n; float el, proizv = 1, sum = 0, min;
+	float* A;
+	cout << "Введите размер массива" << endl;
+	cin >> n;
+	A = (float*)calloc(n, sizeof(float));//выделение памяти для динамического массива
+	for (int i = 0; i < n; i++) {//заполнение массива с клавиатуры
+		cout << "Введите элемент " << endl;
+		cin >> el;
+		A[i] = el;
+		if (i == 0) {
+			min = A[0];
+		}
+		if (A[i] < min)
+			min = A[i];
+		if (el > 0) {
+			proizv = proizv * el;
+		}
+	}
+	cout << "Размер массива (байты) " << (int)_msize(A) << endl;//проверка выделеной памяти
+	for (int i = 0; i < n; i++) {
+		cout << A[i] << " " << endl;
+	}
+	//находим произведение
+	cout << "Произведение= " << proizv << endl;
+	for (int i = 0; i < n; i++) {
+		if (A[i] != min) {
+			sum = sum + A[i];
+		}
+		else
+			break;
+	}
+	//находим сумму
+	cout << "Сумма= " << sum << endl;
+	free(A);
+	
+}
---
+++ lab15/lab15/z2/z2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <stdio.h>	
+#include <stdlib.h>
+
+void main()
+{
+	setlocale(LC_CTYPE, "russian");
+	using namespace std;
+	int stlb, str, ** A, sum;
+	cout << "Введите количество строк " << endl;
+	cin >> str;
+	cout << "Введите количество столбцов " << endl;
+	cin >> stlb;
+
+	A = new int* [str];//создаем матрицу
+
+	for (int i = 0; i < stlb; i++) {
+		A[i] = new int[str];
+		//вводим элементы
+		for (int j = 0; j < str; j++) {
+			cout << "Введите элемент" << endl;
+			cin >> A[i][j];
+		}
+	}
+	for (int i = 0; i < stlb; i++) {//выводим эту матрицу
+		for (int j = 0; j < str; j++)
+			cout << "A[" << i << "][" << j << "]" << A[i][j] << endl;
+	}
+	for (int i = 0; i < stlb; i++) {//нахождение строки и ее суммы
+		int flag = 0; sum = 0;
+		for (int j = 0; j < str; j++) {
+			if (A[i][j] > 0) {
+				flag = 1;//создаем флаг и даем ему значение 1
+			}
+			else {
+				break;
+			}
+			if (flag == 1) {
+				sum = sum + A[i][j];
+			}
+		}
+		if (flag == 1) {
+			cout << "Строка: " << i + 1 << endl;
+			cout << "Сумма: " << sum << endl;
+			break;
+		}
+	}
+	for (int i = 0; i < str; i++) {//уменьшение всех элементов массива на сумму строки
+		for (int j = 0; j < stlb; j++) {
+			A[i][j] -= sum;
+			cout << "A[" << i << "][" << j << "]-сумма= " << A[i][j] << endl;
+		}
+	}
+}
