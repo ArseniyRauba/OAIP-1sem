581a24614a6193793cd71332815372471c0d0b94
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 16:44:03 +0300

    Add files via upload

---
+++ lab5/lab5(2z)/lab5(2z).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "RUS");
+	int a, b;
+	cout << "Введите a ";
+	cin >> a;
+	cout << "Введите b ";
+	cin >> b;
+	cout << "a/b= " << a << "/" << b << endl;
+	if (a < b)
+		cout << "Дробь a/b - правильная";
+	else if (a == b)
+		cout << "1";
+	else
+		cout << "Дробь a/b - неправильная";
+}
---
+++ lab5/lab5/lab5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+int main()
+{
+	setlocale(LC_CTYPE,"RUS");
+	float x, y, x2, a, max, min;
+	printf("Введите x "); scanf_s("%f", &x);
+	printf("Введите y "); scanf_s("%f", &y);
+	printf("Введите x2 "); scanf_s("%f", &x2);
+	max = x;
+	if (y>max) max = y;
+	if (x2>max) max = x2;
+	min = x;
+	if (min > y) min = y;
+	if (min > x2) min = x2;
+	a = max * min;
+	printf("max(x, y, z) * min(x, y, x2) = ");
+	printf("%.2f",a);
+}
